f65695b312256faef64ad103cf92da051472ad74
matthewmillerh <matthewmillerh@outlook.com>
2025-04-15 10:03:01 +0200

    removed phpMailer email info

--- email1.php
+++ email1.php
@@ -18,16 +18,16 @@ $body .= "Questions<br><br>".$_REQUEST['textarea'];
 $mail->IsSMTP();
 $mail->SMTPAuth   = true;                  // enable SMTP authentication
 //$mail->SMTPSecure = "ssl";                 // sets the prefix to the servier
-$mail->Host       = "smtp.mweb.co.za";      // sets GMAIL as the SMTP server
+$mail->Host       = ";      // sets GMAIL as the SMTP server
 $mail->Port       = 25;                   // set the SMTP port for the GMAIL server
-$mail->Username   = "galleryinfo@mweb.co.za";  // GMAIL username
-$mail->Password   = "carsten";            // GMAIL password
+$mail->Username   = "";  // GMAIL username
+$mail->Password   = "";            // GMAIL password
 $mail->SMTPDebug = 1;
 
-$mail->SetFrom('galleryinfo@mweb.co.za', 'Website Order');
+$mail->SetFrom('', '');
 $mail->Subject = "Request to purchase";
-$mail->AddReplyTo('galleryinfo@mweb.co.za', 'Reply');
-$mail->ReturnPath='galleryinfo@mweb.co.za';
+$mail->AddReplyTo('', 'Reply');
+$mail->ReturnPath='';
 $mail->MsgHTML($body);
 
 $address = "matthewmillerh@outlook.com";
